7d9b601a30aaf7ea54d8a0fec59f2bf89475c54a
brandyneve <brandy_neve@cable.comcast.com>
2021-07-16 09:14:02 -0600

    chapter 14 edits enum

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -1,8 +1,6 @@
 package org.launchcode.codingevents.models;
 
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Size;
+import javax.validation.constraints.*;
 import java.util.Objects;
 
 /**
@@ -24,11 +22,31 @@ public class Event {
     @Email(message = "Invalid email. Try again.")
     private String contactEmail;
 
-    public Event(String name, String description, String contactEmail) {
+    @NotBlank(message= "Location is required")
+    @NotNull(message= "Location is required")
+    private String location;
+
+    @AssertTrue(message="You are gonna have to click that, but only because the book says so, but we feel this is a " +
+            "silly validation.")
+    private Boolean registration;
+
+
+    @Min(1)//message="seriously, 0 attendees? why are you even bothering?")
+    private int attendees;
+
+    private EventType type;
+
+    public Event(String name, String description, String contactEmail, String location, Boolean registration,
+                 int attendees, EventType type) {
         this();
         this.name = name;
         this.description = description;
         this.contactEmail = contactEmail;
+        this.location = location;
+        this.attendees = attendees;
+        this.registration = registration;
+        this.type = type;
+
     }
 
     public Event() {
@@ -58,6 +76,42 @@ public class Event {
 
     public void setContactEmail(String contactEmail) {
         this.contactEmail = contactEmail;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public Boolean getRegistration() {
+        return registration;
+    }
+
+    public void setRegistration(Boolean registration) {
+        this.registration = registration;
+    }
+
+    public int getAttendees() {
+        return attendees;
+    }
+
+    public void setAttendees(int attendees) {
+        this.attendees = attendees;
+    }
+
+    public EventType getType() {
+        return type;
+    }
+
+    public void setType(EventType type) {
+        this.type = type;
     }
 
     public int getId() {
